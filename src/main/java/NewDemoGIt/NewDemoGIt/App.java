d6740e736581bc6bd00f42ee380807333a393076
Gaurav Kumar <40858776+gaurav150792@users.noreply.github.com>
2018-10-16 12:38:53 +0530

    First maven project integrated with git hub

---
+++ src/main/java/NewDemoGIt/NewDemoGIt/App.java
@@ -0,0 +1,13 @@
+package NewDemoGIt.NewDemoGIt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
